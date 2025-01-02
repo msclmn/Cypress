f52daa2966df14bc25ffd08580f3336d0f8caee8
Rega <44708903+msclmn@users.noreply.github.com>
2025-01-02 14:58:25 -0600

    Update commands.js

--- support/commands.js
+++ support/commands.js
@@ -1,15 +1,3 @@
-// cypress/support/commands.js
-
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-
 Cypress.Commands.add('navigateTo', (url) => {
     try {
       cy.visit(url);
